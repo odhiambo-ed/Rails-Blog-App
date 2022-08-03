22f1c9e2b46fa4f841298884b00fd6cbfb5d850f
Edward Odhiambo <Edward@Macs-MacBook-Air.local>
2022-08-03 13:16:41 +0300

    Added RSpec

--- config/routes.rb
+++ config/routes.rb
@@ -6,17 +6,18 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'users#index'
 
-  get 'posts/new', to: 'posts#new'
-  get 'comments/new', to: 'comments#new'
+  # get 'posts/new', to: 'posts#new'
+  # get 'comments/new', to: 'comments#new'
 
-  post 'posts/', to: 'posts#create'
-  post 'comments/', to: 'comments#create'
+  # post 'posts/', to: 'posts#create'
+  # post 'comments/', to: 'comments#create'
 
-  post '/users/:user_id/posts/:id/likes', to: 'likes#create'
+  # post '/users/:user_id/posts/:id/likes', to: 'likes#create'
 
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show new create] do
       resources :comments, only: [:create, :new]
+      resources :likes, only: [:create, :new]
     end
   end
   
