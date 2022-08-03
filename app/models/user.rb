1211e1e792ddbad2526edb49bbb5146ba666de5c
Edward Odhiambo <Edward@Macs-MacBook-Air.local>
2022-08-03 15:49:09 +0300

    Fix Errors 5

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-   validates :name, presence: true, allow_blank: false, length: { maximum: 250 }
+    validates :name, presence: true, allow_blank: false, length: { maximum: 250 }
     validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
     has_many :comments, foreign_key: 'author_id'
@@ -9,4 +9,3 @@ class User < ApplicationRecord
     def recent_posts
         posts.order(created_at: :desc).limit(3)
     end
-end
