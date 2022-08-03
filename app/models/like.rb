480faa1aae7980db3b4db5e671233956e1ec6a0d
Edward Odhiambo <Edward@Macs-MacBook-Air.local>
2022-08-03 11:53:24 +0300

    Delete Errors

--- app/models/like.rb
+++
@@ -1,8 +0,0 @@
-class Like < ApplicationRecord
-  belongs_to :author, class_name: 'User', foreign_key: :author_id
-  belongs_to :post, class_name: 'Post', foreign_key: :post_id
-
-  def self.update_likes_counter
-    Post.increment!(:LikesCounter)
-  end
-end
--- db/migrate/20220712180151_update_comments_users_reference.rb
+++
@@ -1,6 +0,0 @@
-class UpdateCommentsUsersReference < ActiveRecord::Migration[7.0]
-  def change
-    remove_reference :comments, :user
-    add_reference :comments, :author, foreign_key: {to_table: :users}
-  end
-end
