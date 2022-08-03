90c861019ee554a1087cc7e9ba182a64b2d6f861
Edward Odhiambo <Edward@Macs-MacBook-Air.local>
2022-08-03 15:42:37 +0300

    Fix Errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
-  # Validations
-  validates :name, presence: true, allow_blank: false, length: { maximum: 250 }
-  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :name, presence: true
-  validates :post_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+    validates :name, presence: true, allow_blank: false, length: { maximum: 250 }
+    validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
+    has_many :comments, foreign_key: 'author_id'
+    has_many :likes, foreign_key: 'author_id'
+    has_many :posts, foreign_key: 'author_id'
   def self.most_recent_posts
     Post.order(created_at: :desc).limit(3)
   end
